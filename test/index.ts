6373543f01ecce7162b27ed6ae67a2495862d33c
kralonur <kralonur1998@gmail.com>
2021-12-15 16:07:03 +0700

    Main functions for test

---
+++ test/index.ts
@@ -0,0 +1,22 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { DAO, DAO__factory } from "../typechain-types/";
+
+describe("DAO", function () {
+
+    let accounts: SignerWithAddress[];
+    let owner: SignerWithAddress;
+    let contract: DAO;
+
+    before(async function () {
+        accounts = await ethers.getSigners();
+        owner = accounts[0];
+    });
+
+    beforeEach(async function () {
+        const tokenFactory = new DAO__factory(owner);
+        contract = await tokenFactory.deploy();
+        await contract.deployed();
+    });
+})
